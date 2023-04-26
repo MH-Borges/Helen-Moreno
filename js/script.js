2d8bc8e95e82b3fe3db73cf5fdd0c4400f024ab3
Matheus Henrique Borges <matheush.borges@hotmail.com>
2023-04-26 13:08:48 -0300

    mudanças área depo
     feito alterações na área de depoimentos da tela index

--- js/script.js
+++ js/script.js
@@ -34,6 +34,23 @@ function Stars() {
 setInterval(blinkingStars, 50);
 setInterval(Stars, 25);
 
+$('.toTop').on('click', function(){
+  $("html, body").animate({ scrollTop: 0 }, 400);
+  $('.foguete').addClass('subindo');
+  setTimeout(() => { 
+    $('.foguete').removeClass('subindo');
+  }, 1000)
+});
+
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 100) {
+    $(".toTop").fadeIn();
+  } else {
+    $(".toTop").fadeOut();
+  }
+});
+
 const cardBlog = document.querySelectorAll('.cardPostBlog');
 cardBlog.forEach(f => f.addEventListener('mouseenter', function() {
   cardBlog.forEach(e => {
@@ -55,35 +72,61 @@ cardBlog.forEach(f => f.addEventListener('mouseleave', function() {
   })
 }))
 
-document.addEventListener( 'DOMContentLoaded', function() {
-  var splide = new Splide( '.splide', {
-    type   : 'loop',
-    perPage: 5,
-    drag: 'free',
-    focus  : 'center',
-    autoplay: true,
+// document.addEventListener( 'DOMContentLoaded', function() {
+//   var splide = new Splide( '.splide', {
+//     type   : 'loop',
+//     perPage: 3,
+//     focus  : 'center',
+//     autoplay: true,
 
-    classes: {
-      arrows: 'splide__arrows hide',
-    },
-  } );
+//     classes: {
+//       arrows: 'splide__arrows teste',
+//     },
+//   } );
   
-  splide.mount();
+//   splide.mount();
+// });
+
+const state = {};
+const carouselListDepos = document.querySelector('.carousel__list');
+const carouselItems = document.querySelectorAll('.carousel__item');
+const elems = Array.from(carouselItems);
+
+carouselListDepos.addEventListener('click', function (event) {
+  var newActive = event.target;
+  var isItem = newActive.closest('.carousel__item');
+
+  if (!isItem || newActive.classList.contains('carousel__item_active')) {
+    return;
+  };
+  
+  update(newActive);
 });
 
-$('.toTop').on('click', function(){
-  $("html, body").animate({ scrollTop: 0 }, 400);
-  $('.foguete').addClass('subindo');
-  setTimeout(() => { 
-    $('.foguete').removeClass('subindo');
-  }, 1000)
-});
+const update = function(newActive) {
+  const newActivePos = newActive.dataset.pos;
 
-$(window).scroll(function () {
-  var scroll = $(window).scrollTop();
-  if (scroll >= 100) {
-    $(".toTop").fadeIn();
-  } else {
-    $(".toTop").fadeOut();
+  const current = elems.find((elem) => elem.dataset.pos == 0);
+  const prev = elems.find((elem) => elem.dataset.pos == -1);
+  const next = elems.find((elem) => elem.dataset.pos == 1);
+  const first = elems.find((elem) => elem.dataset.pos == -2);
+  const last = elems.find((elem) => elem.dataset.pos == 2);
+  
+  current.classList.remove('carousel__item_active');
+  [current, prev, next, first, last].forEach(item => {
+    var itemPos = item.dataset.pos;
+    item.dataset.pos = getPos(itemPos, newActivePos)
+  });
+
+};
+const getPos = function (current, active) {
+  const diff = current - active;
+  if (Math.abs(current - active) > 2) {
+    return -current
   }
-});
+  return diff;
+}
+
+setInterval(() => {
+  elems.find((elem) => elem.dataset.pos == 1).click();
+}, 7500);
