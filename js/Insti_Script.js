1e76dbe94cb3ef0dbf594487df7612c038a67b71
Matheus Henrique Borges <matheush.borges@hotmail.com>
2023-04-27 14:39:56 -0300

    area institucional
    criado a animação do foguete descendo e todo o conteúdo da area institucional

---
+++ js/Insti_Script.js
@@ -0,0 +1,93 @@
+// for another helper function that calculates the exact progress value along a motion path where it'll hit the center of the provided target on the given axis ("y" by default), see https://codepen.io/GreenSock/pen/BaPdrKM
+gsap.registerPlugin(MotionPathPlugin, ScrollTrigger);
+
+gsap.set("#motionSVG", { scale: 0.7, autoAlpha: 1 });
+gsap.set("#foguete", {transformOrigin: "50% 50%"});
+let rotateTo = gsap.quickTo("#foguete", "rotation"),
+    prevDirection = 0;
+
+gsap.to("#motionSVG", {
+  scrollTrigger: {
+    trigger: "#motionPath",
+    start: "5% center",
+    // end: () => "+=" + document.querySelector("#motionPath").getBoundingClientRect().height,
+    end: "93% center",
+    scrub: 0.5,
+    markers: false,
+    onUpdate: self => {
+      if (prevDirection !== self.direction) { // only run this when we're changing direction
+        rotateTo(self.direction === 1 ? 0 : -180);
+        prevDirection = self.direction;
+      }
+    }
+  },
+  ease: pathEase("#motionPath"), // a custom ease that helps keep the tractor centered
+  immediateRender: true,
+  motionPath: {
+    path: "#motionPath",
+    align: "#motionPath",
+    alignOrigin: [0.5, 0.5],
+    autoRotate: 90,
+  }
+});
+
+// helper function that returns and ease that bends time to ensure the tractor stays relatively centered. Requires MotionPathPlugin of course
+function pathEase(path, axis="y", precision=1) {
+		let rawPath = MotionPathPlugin.cacheRawPathMeasurements(MotionPathPlugin.getRawPath(gsap.utils.toArray(path)[0]), Math.round(precision * 12)),
+			useX = axis === "x",
+			start = rawPath[0][useX ? 0 : 1],
+      end = rawPath[rawPath.length - 1][rawPath[rawPath.length-1].length - (useX ? 2 : 1)],
+			range = end - start,
+			l = Math.round(precision * 200),
+			inc = 1 / l,
+			positions = [0],
+			a = [],
+			minIndex = 0,
+			getClosest = p => {
+				while (positions[minIndex] <= p && minIndex++ < l) { }
+				a.push((p - positions[minIndex-1]) / (positions[minIndex] - positions[minIndex - 1]) * inc + minIndex * inc);
+			},
+			i = 1,
+			p, v;
+		for (; i < l; i++) {
+			p = i / l;
+			v = MotionPathPlugin.getPositionOnPath(rawPath, p)[axis];
+			positions[i] = (v - start) / range;
+		}
+		positions[l] = 1;
+		for (i = 0; i < l; i++) {
+			getClosest(i / l);
+		}
+		a.push(1);
+		return p => {
+			let i = p * l,
+				s = a[i | 0];
+			return s + (a[Math.ceil(i)] - s) * (i % 1);
+		}
+	}
+
+
+$('.toTopInsti').on('click', function(){
+  $("html, body").animate({ scrollTop: 0 }, 400);
+  $('.foguete').addClass('subindo');
+  setTimeout(() => { 
+    $('.foguete').removeClass('subindo');
+  }, 750)
+});
+
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 5500) {
+    $(".toTopInsti").addClass('sobe');
+    $(".toTopInsti").fadeIn();
+    setTimeout(() => { 
+      $('.toTopInsti').removeClass('sobe');
+      $('.toTopInsti').addClass('sobePara');
+    }, 1250);
+  } else {
+    
+    $(".toTopInsti").fadeOut();
+    $('.toTopInsti').removeClass('sobePara');
+  }
+});
+  
--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ $('.toTop').on('click', function(){
   $('.foguete').addClass('subindo');
   setTimeout(() => { 
     $('.foguete').removeClass('subindo');
-  }, 1000)
+  }, 750)
 });
 
 $(window).scroll(function () {
@@ -50,27 +50,6 @@ $(window).scroll(function () {
     $(".toTop").fadeOut();
   }
 });
-
-const cardBlog = document.querySelectorAll('.cardPostBlog');
-cardBlog.forEach(f => f.addEventListener('mouseenter', function() {
-  cardBlog.forEach(e => {
-    e.classList.add('menor');
-    e.classList.remove('maior');
-  })
-  this.classList.add('maior');
-  this.querySelector('.icons').classList.add('active')
-
-
-}))
-
-cardBlog.forEach(f => f.addEventListener('mouseleave', function() {
-  cardBlog.forEach(e => {
-    e.classList.remove('menor');
-    e.classList.remove('maior');
-    var icons = e.querySelector('.icons');
-    icons.classList.remove('active');
-  })
-}))
 
 // document.addEventListener( 'DOMContentLoaded', function() {
 //   var splide = new Splide( '.splide', {
@@ -86,47 +65,3 @@ cardBlog.forEach(f => f.addEventListener('mouseleave', function() {
   
 //   splide.mount();
 // });
-
-const state = {};
-const carouselListDepos = document.querySelector('.carousel__list');
-const carouselItems = document.querySelectorAll('.carousel__item');
-const elems = Array.from(carouselItems);
-
-carouselListDepos.addEventListener('click', function (event) {
-  var newActive = event.target;
-  var isItem = newActive.closest('.carousel__item');
-
-  if (!isItem || newActive.classList.contains('carousel__item_active')) {
-    return;
-  };
-  
-  update(newActive);
-});
-
-const update = function(newActive) {
-  const newActivePos = newActive.dataset.pos;
-
-  const current = elems.find((elem) => elem.dataset.pos == 0);
-  const prev = elems.find((elem) => elem.dataset.pos == -1);
-  const next = elems.find((elem) => elem.dataset.pos == 1);
-  const first = elems.find((elem) => elem.dataset.pos == -2);
-  const last = elems.find((elem) => elem.dataset.pos == 2);
-  
-  current.classList.remove('carousel__item_active');
-  [current, prev, next, first, last].forEach(item => {
-    var itemPos = item.dataset.pos;
-    item.dataset.pos = getPos(itemPos, newActivePos)
-  });
-
-};
-const getPos = function (current, active) {
-  const diff = current - active;
-  if (Math.abs(current - active) > 2) {
-    return -current
-  }
-  return diff;
-}
-
-setInterval(() => {
-  elems.find((elem) => elem.dataset.pos == 1).click();
-}, 7500);
